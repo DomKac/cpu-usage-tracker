58e506a5a1976850b675cdf9274d9c4790ac0fd9
DomKac <dominik42001@gmail.com>
2023-06-07 23:50:02 +0200

    Working fscanf

--- cut.c
+++ cut.c
@@ -1,26 +1,33 @@
 #include <stdlib.h>
 #include <stdio.h>
 
-#define PROC_STAT_PATH "/proc/stat"
+#include "cut.h"
 
+#define PROC_STAT_PATH "/proc/stat"
 
 
 int main(void) {
 
     FILE* procs_stat = fopen(PROC_STAT_PATH, "r");
-    char const text[] = "Roger roger";
 
     if (!procs_stat){
         perror("fopen failed");
-        return EXIT_FAILURE ;
+        return EXIT_FAILURE;
     }
 
-    
+    CPU* cpu = new_cpu();
+    printf("sizeof(cpu) = %zu\n", sizeof(cpu));
 
-    
-    fprintf(stderr, "%s\n", text);
+    if (!fscanf(procs_stat, "%s %u %u %u %u %u %u %u %u %u %u\n", cpu->name, &cpu->user, &cpu->nice, &cpu->system, &cpu->idle, &cpu->iowait, &cpu->irq, &cpu->softirq, &cpu->steal, &cpu->guest, &cpu->guest_nice))
+    {
+        perror("fscanf failed");
+        return EXIT_FAILURE;
+    }
+
+    print_cpu_info(cpu);
 
     fclose(procs_stat);
+    free(cpu);
 
     return EXIT_SUCCESS;
 }
--- cut.h
+++ cut.h
@@ -1,9 +1,9 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-
-typedef struct cpu
+typedef struct CPU
 {
+    char name[8];
     unsigned user;
     unsigned nice;
     unsigned system;
@@ -14,4 +14,29 @@ typedef struct cpu
     unsigned steal;
     unsigned guest;
     unsigned guest_nice;
-} cpu;
+} CPU;
+
+CPU* new_cpu(void);
+void print_cpu_info(const CPU* cpu);
+
+CPU* new_cpu(void) {
+    CPU* cpu = (CPU*)malloc(sizeof(CPU));
+    return cpu;
+}
+
+void print_cpu_info(const CPU* cpu) {
+    printf("%s ", cpu->name);
+    printf("%u ", cpu->user);
+    printf("%u ", cpu->nice);
+    printf("%u ", cpu->system);
+    printf("%u ", cpu->idle);
+    printf("%u ", cpu->iowait);
+    printf("%u ", cpu->irq);
+    printf("%u ", cpu->softirq);
+    printf("%u ", cpu->steal);
+    printf("%u ", cpu->guest);
+    printf("%u\n", cpu->guest_nice);
+}
+
+
+
